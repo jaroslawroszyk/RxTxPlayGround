e5a1e20e0fa3137718222353b7ba3b65c818205b
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2025-07-14 22:27:25 +0200

    1

---
+++ client/src/main.rs
@@ -0,0 +1,18 @@
+// #[tokio::main]
+fn main() // -> Result<(), Box<dyn Error>> {
+{
+    // let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    // println!("Server listening on 127.0.0.1:8080");
+
+    // loop {
+    //     let (socket, addr) = listener.accept().await?;
+    //     println!("New client: {}", addr);
+
+    //     tokio::spawn(async move {
+    //         if let Err(e) = handle_client(socket).await {
+    //             eprintln!("Error handling client {}: {:?}", addr, e);
+    //         }
+    //         println!("Client {} disconnected", addr);
+    //     });
+    // }
+}
